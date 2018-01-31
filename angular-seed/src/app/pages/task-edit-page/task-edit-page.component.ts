460f9b2ed8ff0fea42c1005e49e4fa6d8a59686a
juanc970410 <juan.c.herrerav@hotmail.com>
2018-01-31 16:26:35 -0500

    lab1

--- angular-seed/src/app/pages/task-edit-page/task-edit-page.component.ts
+++ angular-seed/src/app/pages/task-edit-page/task-edit-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-task-edit-page',
@@ -6,13 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-edit-page.component.css']
 })
 export class TaskEditPageComponent implements OnInit {
-  constructor(
-  ) {
-
-  }
+    private todoForm: FormGroup  
+    constructor(
+public todoService: TodoService,
+public formBuilder: FormBuilder,
+public router: Router) {}
 
   ngOnInit() {
-
+    this.todoForm = this.formBuilder.group({
+      description: '',
+      completed: '',
+      priority: ''
+    });
   }
+  onSubmit() {
+   this.todoService.create(
+     this.todoForm.get('description').value,
+     this.todoForm.get('priority').value,
+     Boolean(this.todoForm.get('completed').value)
+   );
+   this.router.navigate(['/tasks']);
+ }
 
 }
---
+++ angular-seed/src/app/services/todo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo';
+
+@Injectable()
+export class TodoService {
+    private todos: Todo[] = [
+        new Todo('todo 1'),
+        new Todo('todo 2', 1, true),
+        new Todo('todo 3')
+    ];
+    constructor() {}
+    
+    list(): Todo[] {
+        return this.todos;
+    }
+
+    create(description: string, priority: Number, completed: boolean):void{
+        this.todos.push(new Todo(description,priority,completed));
+}
+}
